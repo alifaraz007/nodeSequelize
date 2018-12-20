1007ed4c21dfc52d323d56375ab46626048820bd
pharaj <pharaj@excellencetechnologies.in>
2018-12-20 18:08:51 +0530

    remove extra function and not saving token in header

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -3,34 +3,30 @@ const db = require('../config/database')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res, next) => {
-    (async () => {
-        try {
-            const user = await userProvider.create(req, res)
-            const result = await db.User.create(user)
-            res.json(result);
-        } catch (err) {
-            res.status(400).json(err);
-        }
-    })();
+    try {
+        const user = await userProvider.create(req, res)
+        const result = await db.User.create(user)
+        res.json(result);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
 
 const login = async (req, res) => {
-    (async () => {
-        try {
-            const user = await userProvider.log(req, res)
-            const result = await db.User.findOne({
-                where: { email: user.email, password: user.password }
-            })
-            if (!result) {
-                res.json('entered wrong email or password');
-            } else {
-                let token = jwt.sign({ token: result.id }, 'secret_key', { expiresIn: 60 * 60 })
-                res.header('x-auth-token', token).json({ status: 1, token: token, })
-            }
-        } catch (err) {
-            res.status(400).json(err);
+    try {
+        const user = await userProvider.log(req, res)
+        const result = await db.User.findOne({
+            where: { email: user.email, password: user.password }
+        })
+        if (!result) {
+            res.json('entered wrong email or password');
+        } else {
+            let token = jwt.sign({ token: result.id }, 'secret_key', { expiresIn: 60 * 60 })
+            res.json({ status: 1, token: token, })
         }
-    })();
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
 
 module.exports = {
